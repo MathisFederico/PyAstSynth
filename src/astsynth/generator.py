d85dc20012b3000300a6f907a4012581335b4894
AutoMathïs <automathis@protonmail.com>
2024-07-27 13:22:20 +0200

    ✅ Test restriction to output type

--- src/astsynth/generator.py
+++ src/astsynth/generator.py
@@ -1,15 +1,22 @@
 import ast
-from typing import Any, Generator, Optional
+from typing import Any, Generator, Optional, Type
 
 
 class CodeGenerator:
-    def __init__(self, allowed_constants: Optional[dict[str, Any]] = None) -> None:
+    def __init__(
+        self,
+        allowed_constants: Optional[dict[str, Any]] = None,
+        output_type: Type[object] = object,
+    ) -> None:
         if allowed_constants is None:
             allowed_constants = {}
         self.allowed_constants = allowed_constants
+        self.output_type = output_type
 
     def enumerate(self) -> Generator[ast.FunctionDef, None, None]:
         for constant_name, const in self.allowed_constants.items():
+            if not isinstance(const, self.output_type):
+                continue
             yield ast.FunctionDef(
                 name=f"const_{const}",
                 body=[ast.Return(ast.Name(constant_name))],
--- tests/test_ast_restrictions.py
+++ tests/test_ast_restrictions.py
@@ -1,6 +1,6 @@
 import ast
 import astor
-from typing import Any
+from typing import Any, Type
 import pytest
 
 from astsynth.generator import CodeGenerator
@@ -20,6 +20,15 @@ def test_return_constant(generation_fixture: "CodeGenerationFixture"):
     )
 
 
+def test_restrict_to_expected_type(generation_fixture: "CodeGenerationFixture"):
+    generation_fixture.given_expected_output_type(int)
+    generation_fixture.given_allowed_constants({"N": 42, "A": "a"})
+    generation_fixture.when_enumerating_generation()
+    generation_fixture.then_generated_functions_asts_should_be(
+        set([function_ast_from_source("def const_42():\n    return N")])
+    )
+
+
 def function_ast_from_source(source: str):
     return ast.parse(source).body[0]
 
@@ -36,6 +45,9 @@ class CodeGenerationFixture:
 
     def given_allowed_constants(self, constants: dict[str, Any]):
         self.code_generator.allowed_constants = constants
+
+    def given_expected_output_type(self, expected_output_type: Type[object]):
+        self.code_generator.output_type = expected_output_type
 
     def when_enumerating_generation(self):
         for program_tree in self.code_generator.enumerate():
